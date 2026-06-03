d44e2a25f4f4bfe412a5a637d765f7d03900879f
Tommy Eareast <tommyeareast@gmail.com>
2026-06-03 14:39:18 +1000

    Feat: Refactor Page Swiping Effect

--- src/lib/LanguageTransition.tsx
+++ src/lib/LanguageTransition.tsx
@@ -8,17 +8,14 @@ export default function LanguageTransition({
     children: React.ReactNode;
 }) {
     const { language, isTransitioning } = useLanguage();
+    const direction = language === "en" ? "left" : "right";
 
     return (
         <div
             className={`transition-all duration-500 ease-out will-change-transform ${
-                language === "en"
-                    ? isTransitioning
-                        ? "en-left"
-                        : "en-right"
-                    : isTransitioning
-                      ? "zh-left"
-                      : "zh-right"
+                isTransitioning
+                    ? `swipe-out-${direction}`
+                    : `swipe-in-${direction}`
             }`}
         >
             {children}
